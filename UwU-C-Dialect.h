a520c6dc4978deb95d4cf8a5324fb7b03b1aa7bf
LateNightProgramming <124677881+LateNightProgramming@users.noreply.github.com>
2023-08-18 11:48:56 +1000

    Create UwU-C-Dialect.h

---
+++ UwU-C-Dialect.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define If if(
+#define Do ){
+#define Finish }
+#define Big_Memowy_UwU double
+#define My_Age:) int
+#define Facts_UwU true
+#define Cap_-W- false
+#define Small_Memowy_UwU float
+#define Sooooo_BIG_UwU >
+#define Small_-W- <
+#define tip main
+#define  UwU ;
+#define WholeNumber int
+#define speak printf
+#define CUM return
+#define Open {
+#define Else else
+#define While while(
+#define For for(
+#define  increaseInches ++
+#define Slide goto
+#define endLine "\n"
+#define Office_Chair char
